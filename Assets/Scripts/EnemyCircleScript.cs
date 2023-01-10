fd0c07057193d29fe0e1ef25069b735dd14464ed
Aditya Naidu <adityanaidu16344@gmail.com>
2023-01-10 12:20:45 -0600

    enemy random projectile

--- Assets/Scripts/EnemyCircleScript.cs
+++ Assets/Scripts/EnemyCircleScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class circlescript : MonoBehaviour
+public class EnemyCircleScript : MonoBehaviour
 {
     public GameObject player;
     public GameObject target;
@@ -34,7 +34,7 @@ public class circlescript : MonoBehaviour
         fire = false;
         playerOneTurn = true;
         //so it doesnt move initially
-        GetComponent<Rigidbody2D>().constraints=RigidbodyConstraints2D.FreezePosition;
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
     }
 
 
@@ -43,7 +43,7 @@ public class circlescript : MonoBehaviour
     public void onStart()
     {
         fire = false;
-        transform.position=current;
+        transform.position = current;
         GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
     }
 
@@ -51,40 +51,40 @@ public class circlescript : MonoBehaviour
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-      
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetKeyDown("space"))
         {
             transform.position = current;
             trajOn = true;
 
             //checking the amt of turns the player/ target has
-            if (player.GetComponent<basicsOfObjects>().turn)
-                player.GetComponent<basicsOfObjects>().amtOfTurns--;
-            else
+            if (target.GetComponent<basicsOfObjects>().turn)
                 target.GetComponent<basicsOfObjects>().amtOfTurns--;
+            else
+                player.GetComponent<basicsOfObjects>().amtOfTurns--;
         }
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetKeyUp("space"))
         {
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
             fire = true;
             playerOneTurn = false;
-            heightMult = 4 * (player.transform.position.y - mousePos.y);
-            targMult = 8 * (player.transform.position.x - mousePos.x);
+            heightMult = Random.Range(4.7f, 6.5f);
+            targMult = Random.Range(8.5f, 20f);
             // Debug.Log(heightMult);
-            // Debug.Log(targMult);
+            //Debug.Log(targMult);
             //Debug.Log(target.transform.position.x);
-            
-            
+
+
         }
 
         if (fire)
         {
-            
-            playerX = player.transform.position.x;
-            targetX = targMult-7;
+
+            playerX = target.transform.position.x;
+            targetX = 9 - targMult;
             dist = targetX - playerX;
             nextX = Mathf.MoveTowards(transform.position.x, targetX, speed * Time.deltaTime);
-            baseY = Mathf.Lerp(player.transform.position.y, target.transform.position.y, (nextX - playerX) / dist);
+            baseY = Mathf.Lerp(target.transform.position.y, player.transform.position.y, (nextX - playerX) / dist);
             height = heightMult * (nextX - playerX) * (nextX - targetX) / (-0.25f * dist * dist);
 
             movePosition = new Vector3(nextX, baseY + height, transform.position.z);
